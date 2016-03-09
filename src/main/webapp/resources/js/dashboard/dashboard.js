ba7b81f120346a1a9aac6a9bf8fdbb9ba6a04723
esperyong <esperyong@gmail.com>
2016-03-09 17:26:33 +0800

    write reactjs echarts and table components

--- src/main/webapp/resources/js/dashboard/dashboard.js
+++ src/main/webapp/resources/js/dashboard/dashboard.js
@@ -1,3 +1,81 @@
+var react_echarts = {};
+
+(function (ns) {
+
+    //private scoping
+    var global;
+
+    ns.ECharts = React.createClass({
+
+        displayName: "ECharts",
+
+        filterMap: function(filterArray,props){
+          const options = {};
+          filterArray.map((key) => {
+            const option = props[key];
+            if (option !== undefined) {
+              options[key] = option;
+            }
+          });
+          return options;
+        },
+
+        componentDidMount: function() {
+          const { onReady } = this.props;
+          this.initChart();
+          if (onReady) onReady(this.chart);
+        },
+
+        componentDidUpdate: function(prevProps) {
+          this.reRenderChart(this.props);
+        },
+
+        componentWillUnmount: function() {
+          this.chart.dispose();
+        },
+
+        reRenderChart: function(chart_options) {
+          this.chart.setOption(chart_options, chart_options.theme);
+        },
+
+        initChart: function() {
+          // 指定图表的配置项和数据
+          const node = this.refs.chart;
+          const options = this.filterMap([
+            'backgroundColor',
+            'animation',
+            'calculable',
+            'renderAsImage',
+            'timeline',
+            'title',
+            'toolbox',
+            'tooltip',
+            'legend',
+            'dataRange',
+            'dataZoom',
+            'roamController',
+            'grid',
+            'color',
+            'xAxis',
+            'yAxis',
+            'series',
+          ], this.props);
+          this.chart = echarts.init(node);
+          this.chart.setOption(options, this.props.theme);
+        },
+
+        render: function() {
+          return (
+              <div ref="chart" className={this.props.className} style={ this.props.chartStyle }>
+              </div>
+          );
+        }
+
+    });
+
+})(react_echarts);
+
+
 var QueryBoardUI = React.createClass({
 
     render: function() {
@@ -102,25 +180,46 @@ var QueryBoard = React.createClass({
           <div className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
               <ChartsPanel />
               <QueryBar />
-              <ResultTable />
+              <SimpleResultTable />
           </div>
         );
     }
 
 });
 
+
 var ChartsPanel = React.createClass({
 
     render: function() {
-        return (
 
+          // 指定图表的配置项和数据
+          var chartConfigs = {
+              chartStyle: {width:"600px", height:"400px"},
+              className:"img-responsive",//can delete if required
+              title: {
+                  text: 'ECharts 入门示例'
+              },
+              tooltip: {},
+              legend: {
+                  data:['销量']
+              },
+              xAxis: {
+                  data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+              },
+              yAxis: {},
+              series: [{
+                  name: '销量',
+                  type: 'bar',
+                  data: [5, 20, 36, 10, 10, 20]
+              }]
+          };
+
+        return (
           <div>
           <h1 className="page-header">Dashboard</h1>
           <div className="row placeholders">
             <div className="col-xs-6 col-sm-3 placeholder">
-              <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" className="img-responsive" alt="Generic placeholder thumbnail"/>
-              <h4>Label</h4>
-              <span className="text-muted">Something else</span>
+              <react_echarts.ECharts {...chartConfigs} />
             </div>
             <div className="col-xs-6 col-sm-3 placeholder">
               <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" className="img-responsive" alt="Generic placeholder thumbnail"/>
@@ -154,136 +253,70 @@ var QueryBar = React.createClass({
 
 });
 
-var ResultTable = React.createClass({
+var SimpleResultTableHead = React.createClass({
+
+    render: function() {
+
+        var theads = [];
+        this.props.theads.forEach(function(head,key){
+            theads.push(<th key={key}>{head}</th>);
+        }); 
+
+        return (
+            <thead>
+                <tr>
+                   {theads}
+                </tr>
+            </thead>
+        );
+
+    }
+
+});
+
+var SimpleResultTableBody = React.createClass({
+
+
+    render: function() {
+        var rows = [];
+
+        this.props.rowDatas.forEach(function(rowData,key){
+
+            var cells = [];
+            rowData.forEach(function(cellData,key){
+                cells.push(<td key={key}>{cellData}</td>)
+            });
+
+            rows.push(<tr key={key}>{cells}</tr>);
+        }); 
+
+        return (
+          <tbody>{rows}</tbody>
+        );
+    }
+
+});
+
+var SimpleResultTable = React.createClass({
     render: function() {
         return (
+
           <div className="table-responsive">
             <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td>nostra</td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td>libero</td>
-                </tr>
-              </tbody>
+              <SimpleResultTableHead theads={['指标项','指标值','采集时间']} />
+              <SimpleResultTableBody rowDatas={[
+                  ['CPU','53.01%','2015-12-14 22:22:00'],
+                  ['CPU','53.01%','2015-12-14 22:22:00'],
+                  ['CPU','53.01%','2015-12-14 22:22:00'],
+                  ['CPU','53.01%','2015-12-14 22:22:00'],
+                  ['CPU','53.01%','2015-12-14 22:22:00'],
+                  ['CPU','53.01%','2015-12-14 22:22:00'],
+                  ['CPU','53.01%','2015-12-14 22:22:00'],
+                  ['CPU','53.01%','2015-12-14 22:22:00'],
+              ]} />
             </table>
           </div>
+
         );
     }
 
@@ -293,6 +326,5 @@ ReactDOM.render(
   <QueryBoardUI />,
   document.getElementById('content')
 );
-
 
 
